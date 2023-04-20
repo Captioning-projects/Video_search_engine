5de259e376f00986813bb9387490daaadf8b023a
Prathamesh Jadhav <55660103+lollinng@users.noreply.github.com>
2023-04-20 18:50:28 +0530

    Update routes.py

--- application/routes.py
+++ application/routes.py
@@ -15,5 +15,14 @@ def home():
 
     return render_template('home.html')
 
+@app.route('/receive_json, methods=['POST'])
+def receive_json():
+    json_data = request.get_json()
+    video_id = json_data['video_id']
+    video_caption = json_data['video_caption']
+    with open('specific_text_file.txt', 'a') as f:
+        f.write(f'{video_id}, {video_caption}\n')
+    return 'JSON sent and processed successfully'
+
 
     
