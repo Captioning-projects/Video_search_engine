b202a628bdf39a2722053eb4b324434a5533275c
Prathamesh Jadhav <55660103+lollinng@users.noreply.github.com>
2023-04-20 19:06:41 +0530

    Update routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 import os
 from flask import render_template, redirect, flash, request
 from application import app
-
+import pandas 
 from application.output import run_query
 
 
@@ -20,8 +20,11 @@ def receive_json():
     json_data = request.get_json()
     video_id = json_data['video_id']
     video_caption = json_data['video_caption']
-    with open('/home/projects/Video_search_engine/output/vid_results.csv', 'a') as f:
-        f.write(f'{video_id}, {video_caption}\n')
+    csv_loc = '/home/projects/Video_search_engine/output/vid_results.csv'
+    df = pd.read_csv(csv_loc)
+    index_ = df.tail(1).index[0]+1
+    df.loc[index_] = [video_caption, video_id]
+    df.to_csv(csv_loc)
     return 'JSON sent and processed successfully'
 
 
