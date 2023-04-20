630d0644bff5e5a9cc5779c5a41b226bd2021fbd
Prathamesh Jadhav <55660103+lollinng@users.noreply.github.com>
2023-04-21 00:55:43 +0530

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -3,10 +3,10 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '75f99765ab52e13194e112135e67e46cbac2d56f'
-# cors = CORS(app, resources={r"/api/*": {"origins": "*",
-#                                         "methods": ["GET", "POST"],
-#                                         }},
-#             headers=["Content-Type", "Authorization"])
+cors = CORS(app, resources={r"/receive_json/": {"origins": "*",
+                                        "methods": ["GET", "POST"],
+                                        }},
+            headers=["Content-Type", "Authorization"])
 
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
