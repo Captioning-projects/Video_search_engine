16df5ce09e7bae10e2acc60472f40e11089c9b96
Prathamesh Jadhav <55660103+lollinng@users.noreply.github.com>
2023-04-21 00:41:17 +0530

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '75f99765ab52e13194e112135e67e46cbac2d56f'
-
+cors = CORS(app, resources={r"/api/*": {"origins": "*",
+                                        "methods": ["GET", "POST"],
+                                        }},
+            headers=["Content-Type", "Authorization"])
 
 from application import routes
